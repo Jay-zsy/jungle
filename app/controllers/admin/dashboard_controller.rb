6675cc2addccab5dd7f3d602e5a7ad1e693a2d9b
Jay Zhou <hollowic@hotmail.com>
2020-02-05 18:06:59 -0800

    finished feature/admin-categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["username"], password: ENV["password"]
+
   def show
     @product_count = Product.all.count
     @categories_count = Category.all.count
